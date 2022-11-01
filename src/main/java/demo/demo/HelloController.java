6196693557b191a1df03a7755eada9befe40cb66
DroWWarD <chernov_da@itgit.ru>
2022-11-01 21:05:04 +0300

    done

--- src/main/java/demo/demo/HelloController.java
+++ src/main/java/demo/demo/HelloController.java
@@ -1,9 +1,13 @@
 package demo.demo;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -23,10 +27,7 @@ public class HelloController {
     private Text country;
 
     @FXML
-    private Text currencyExchangeRate;
-
-    @FXML
-    private Text currencyExchangeRate1;
+    private Text currency;
 
     @FXML
     private TextField city;
@@ -60,24 +61,36 @@ public class HelloController {
 
     @FXML
     void initialize() {
+        //Ловим нажатие на кнопку "Узнать погоду"
         getData.setOnAction(event -> {
             // Получаем данные из текстового поля
-            String getUserCity = city.getText().trim();
-            if(!getUserCity.equals("")) { // Если данные не пустые
-                // Получаем данные о погоде с сайта openweathermap
-                String output = getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + getUserCity + "&appid=5fce0ef7b8c90e429f0fe6cd33103872&units=metric&lang=ru");
+            String getCity = city.getText().trim();
+            if(!getCity.equals("")) { // Данные введены
+                // Получаем данные о погоде с OpenWeatherMap
+                String output = getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + getCity + "&appid=5fce0ef7b8c90e429f0fe6cd33103872&units=metric&lang=RU");
                 System.out.println(output);
-
-                if (!output.isEmpty()) { // Нет ошибки и такой город есть
-
+                if (!output.isEmpty()) { //Введено корректное название города
                     JSONObject obj = new JSONObject(output);
-                    // Обрабатываем JSON и устанавливаем данные в текстовые надписи
-                    weather.setText(obj.getJSONArray("weather").getJSONObject(0).getString("description") +
-                            obj.getJSONObject("sys").getString("country"));
-//                    temp_feels.setText("Ощущается: " + obj.getJSONObject("main").getDouble("feels_like"));
-//                    temp_max.setText("Максимум: " + obj.getJSONObject("main").getDouble("temp_max"));
-//                    temp_min.setText("Минимум: " + obj.getJSONObject("main").getDouble("temp_min"));
-//                    pressure.setText("Давление: " + obj.getJSONObject("main").getDouble("pressure"));
+                    // Парсим JSON, добавляем к тестовым полям полученные данные
+                    weather.setText("ПОГОДА: " + obj.getJSONArray("weather").getJSONObject(0).getString("description").toUpperCase());
+                    time.setText("ТЕКУЩЕЕ ВРЕМЯ: " + (((LocalTime.now().getHour()-3)+(obj.getInt("timezone")/3600))<10?"0":"") +
+                            (((LocalTime.now().getHour()-3)+(obj.getInt("timezone")/3600))==24?"00": ((LocalTime.now().getHour()-3)+(obj.getInt("timezone")/3600)))+
+                            ":" + (LocalTime.now().getMinute()<10? "0":"") + LocalTime.now().getMinute());
+                    String countryAbr = obj.getJSONObject("sys").getString("country");
+                    for (int i = 0; i < Countries.STRINGS.length; i++) {
+                        if (Countries.STRINGS[i].contains(" " + countryAbr + " ")){
+                            String[] countryParse = Countries.STRINGS[i].split(" " + countryAbr + " ");
+                            country.setText("СТРАНА: " + countryParse[0]);
+                            currency.setText("МЕСТНАЯ ВАЛЮТА: " + countryParse[1]);
+                        }
+                    }
+                    temp.setText("ТЕМПЕРАТУРА: " + Math.round(obj.getJSONObject("main").getDouble("temp")) + (char)176);
+                    tempFeelsLike.setText("ОЩУЩАЕТСЯ КАК: " + Math.round(obj.getJSONObject("main").getDouble("feels_like")) + (char)176);
+                    tempMin.setText("МИНИМАЛЬНАЯ ЗА СЕГОДНЯ: " + Math.round(obj.getJSONObject("main").getDouble("temp_min")) + (char)176);
+                    tempMax.setText("МАКСИМАЛЬНАЯ ЗА СЕГОДНЯ: " + Math.round(obj.getJSONObject("main").getDouble("temp_max")) + (char)176);
+                    pressure.setText("ДАВЛЕНИЕ: " + obj.getJSONObject("main").getInt("pressure") + " mmhg");
+                    humidity.setText("ВЛАЖНОСТЬ: " + obj.getJSONObject("main").getInt("humidity") + " %");
+
                 }
             }
         });
@@ -87,6 +100,7 @@ public class HelloController {
         StringBuffer content = new StringBuffer();
 
         try {
+            System.out.println(urlAdress);
             URL url = new URL(urlAdress);
             URLConnection urlConn = url.openConnection();
 
